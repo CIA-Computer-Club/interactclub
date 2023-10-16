81660acc9e36205b2b84778d0ecdd777449d88b7
Sliden101 <chessplayz11@gmail.com>
2023-10-17 00:32:00 +0700

    margin

--- app/page.tsx
+++ app/page.tsx
@@ -23,33 +23,39 @@ export default function IndexPage() {
            alt: "bunch of kids",
         },
      ];
-     
+
      const images1 = [
         {
            src: "https://media.discordapp.net/attachments/1163526744570081310/1163526942541226094/movie0.jpg?ex=653fe5e8&is=652d70e8&hm=11ca17b193749be8a27e4bd0adafa657cc27a62329ebadbc49fad2f6f81178f8&=&width=704&height=396",
-           width: 625,
+           width: 480,
            height: 324,
            alt: "theanlay & soty",
         },
         {
            src: "https://media.discordapp.net/attachments/1163526744570081310/1163526942847422564/movie1.jpg?ex=653fe5e8&is=652d70e8&hm=65fdfd8d96afd70c939e8bd5562b998a2e43f6fe13f3a0e6ef4d873a882a2297&=&width=704&height=396",
-           width: 580,
+           width: 326,
            height: 324,
            alt: "4 girls",
         },
         {
            src: "https://media.discordapp.net/attachments/1163526744570081310/1163526943082283119/movie2.jpg?ex=653fe5e8&is=652d70e8&hm=38598a498c8d2d5068b39707ab6b49563d40d0f485285df19e964928c9c48fcc&=&width=704&height=396",
-           width: 580,
+           width: 420,
            height: 324,
            alt: "bunch of kids",
         },
+        {
+            src: "https://media.discordapp.net/attachments/1163526744570081310/1163526943082283119/movie2.jpg?ex=653fe5e8&is=652d70e8&hm=38598a498c8d2d5068b39707ab6b49563d40d0f485285df19e964928c9c48fcc&=&width=704&height=396",
+            width: 522,
+            height: 324,
+            alt: "bunch of kids",
+         } 
      ];
 
 
 
     return (
         <div className="divide-y divide-gray-400 dark:divide-gray-700">
-            <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+            <div className="space-y-2 pt-6 pb-8 md:space-y-[0.1]">
                 <h1 className="text-3xl font-semibold text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
                     INTERACT <span className="text-[#7096d1]">CLUB</span>
                 </h1>
