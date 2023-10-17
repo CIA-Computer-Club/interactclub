ba1aa0c560ae9b1bac97899f10af34f29a305d0e
Sliden101 <chessplayz11@gmail.com>
2023-10-17 22:51:35 +0700

    feat: "responsive" feat: "people"

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,21 @@ import { row0 , row1 } from "@/data/images.tsx"
 export default function IndexPage() {
 
     return (
-        <div className="divide-y divide-gray-400 dark:divide-gray-700">
-            <div className="space-y-2 pt-6 pb-8 md:space-y-[0.1]">
-                <h1 className="text-3xl font-semibold text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                    INTERACT <span className="text-[#7096d1]">CLUB</span>
-                </h1>
-                <div className="justify-center">
-                    <Gallery images={row0} margin={20} rowHeight={400} />
+        <div className="flex flex-col mt-8">
+            <div className="flex flex-wrap justify-center text-center">
+                <div className="w-full md:w-full lg:w-full">
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                        <h1 className="text-3xl font-semibold text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                            INTERACT <span className="text-[#7096d1]">CLUB</span>
+                        </h1>
+                    </a>
+                    <div className="justify-center">
+                        <Gallery images={row0} margin={20} rowHeight={400} />
+                    </div>
+                    <div className="justify-center">
+                        <Gallery images={row1} margin={20} rowHeight={400} />
+                    </div>
                 </div>
-                <div className="justify-center">
-                    <Gallery images={row1} margin={20} rowHeight={400} />
-                </div>
-
-
             </div>
             <h1 className="text-base font-semibold text-center leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:base sm:leading-10 md:base md:leading-14">
                 The Interact Club is a student-led club that aims to help those in need and urge others to do the same.
