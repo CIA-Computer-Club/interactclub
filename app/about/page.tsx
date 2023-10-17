8530fcafacb4509897ee8b5fc5cdc6c04fdd3c53
Sliden101 <chessplayz11@gmail.com>
2023-10-17 22:27:26 +0700

    d

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,24 +17,40 @@ export default function About(){
           <p className="text-center text-2xl font-light italic mb-6">leaders of year 2023-2024</p>
           <div className="flex flex-wrap">
             <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Vice-President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Treasurer"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Media Manager"} name={"Monita"}/>
           </div>
         <p className="text-center text-2xl font-light italic mb-6">leaders of year 2022-2023</p>
           <div className="flex flex-wrap">
             <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Vice-President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Treasurer"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Media Manager"} name={"Monita"}/>
           </div>
         <p className="text-center text-2xl font-light italic mb-6">leaders of year 2021-2022</p>
           <div className="flex flex-wrap">
-          <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
-            <ProfileCard src={""} position={"President"} name={"Monita"}/>
+            <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Vice-President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Treasurer"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Media Manager"} name={"Monita"}/>
           </div>
+          <p className="text-center text-2xl font-light italic mb-6">leaders of year 2020-2021</p>
+          <div className="flex flex-wrap">
+            <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Vice-President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Treasurer"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Media Manager"} name={"Monita"}/>
+          </div>
+          <p className="text-center text-2xl font-light italic mb-6">leaders of year 2018-2019u </p>
+          <div className="flex flex-wrap">
+            <ProfileCard src={"https://media.discordapp.net/attachments/1163526744570081310/1163822215947624488/image.png?ex=6540f8e7&is=652e83e7&hm=54542708fc33fe30370a7d276b07dd9ed9a0184fe77deb6d3719f4386f43d51c&=&width=400&height=400"} position={"President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Vice-President"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Treasurer"} name={"Monita"}/>
+            <ProfileCard src={""} position={"Media Manager"} name={"Monita"}/>
+          </div>
+
+          
       </div>
     )
 }
