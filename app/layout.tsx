7b5db009f9975ca122dd4b3ff443da24aa4919a8
Sliden101 <chessplayz11@gmail.com>
2023-10-18 20:16:43 +0700

    promote

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import { Providers } from "@/components/Providers";
 import "./globals.css";
 import { EB_Garamond } from "next/font/google";
 
@@ -22,12 +21,10 @@ export default function RootLayout({
       <body
         className={` ${eb_garamond.className} bg-[#fffcf9] text-[#081f3c] dark:bg-[#121212] dark:selection:bg-[#334eac] dark:text-white h-full selection:bg-[#d0e3ff]`}
       >
-        <Providers>
           <Navbar />
           <main className="mx-auto px-4 sm:px-6 lg:px-8">
             {children}
           </main>
-        </Providers>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import Themebutton from "./ThemeButton";
+import Link from "@/components/Link";
 
 import SocialIcon from '@/components/social-icons';
 import siteMetadata from '@/data/siteMetadata'
@@ -21,9 +20,11 @@ export default function Navbar() {
 
           <div className ="flex justify-between items-center">
             
-            <Link href="/doing"><p className="text-xl font-bold italic drop-shadow-lg">what we do</p></Link>
-            <span className="mx-4 mb-6 text-5xl">.</span>
-            <Link href="/about"><p className="mr-4 text-xl font-bold italic drop-shadow-lg">about us</p></Link>
+            <Link className="text-xl font-bold italic drop-shadow-lg" href="/doing">what we do</Link>
+            <Link className="mx-4 mb-7 text-5xl" href="https://github.com/CIA-Computer-Club/interactclub" target="_blank">
+              <span className="">.</span>
+            </Link>
+            <Link className="mr-4 text-xl font-bold italic drop-shadow-lg" href="/about">about us</Link>
           </div>
         </div>
       </div>
