c0e845481f417d779ac95efdfc7c5050d950ec26
Sliden101 <chessplayz11@gmail.com>
2023-10-20 11:15:48 +0700

    fix: border color

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -10,7 +10,7 @@ const ProfileCard: React.FC<Props> = ({src, name, position}) => {
     return (
         <div className="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
             <div className="flex flex-col">
-                <Image className="border-4 border-black"
+                <Image className="border-4 border-[#7096d1]"
                     src={src} 
                     width={400}
                     height={400} 
